f1746006437b3f423353a3fd370e3e0b55f4030c
GURUMOORTHI-J <110287696+GURUMOORTHI-J@users.noreply.github.com>
2025-12-26 09:56:16 +0530

    old connection enabled

--- db/db.js
+++ db/db.js
@@ -2,24 +2,24 @@ const {Pool} = require('pg')
 require('dotenv').config();
 
 
-// const pool  = new Pool({
-//     user:process.env.DB_USER,
-//     host:process.env.DB_HOST,
-//     database:process.env.DB_NAME,
-//     password:process.env.DB_PASSWORD,
-//     port:process.env.DB_PORT,
-//     ssl: { rejectUnauthorized: false },
-//   family: 4 // force IPv4
+const pool  = new Pool({
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false },
+  family: 4 // force IPv4
 
 
-// })
+})
 
-const pool = new Pool({
-  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// const pool = new Pool({
+//   connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
     // user:'postgres',
     // host:'localhost',
